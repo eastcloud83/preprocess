dda8c7117086091ae138c0a436196e91d9bf184e
eastcloud83 <is0412xx@ed.ritsumei.ac.jp>
2020-05-25 15:16:49 +0900

    [fix] 出力ファイルの形を少し変更

--- wakatigaki_mecab.py
+++ wakatigaki_mecab.py
@@ -6,7 +6,10 @@ file_path = input("ファイルパス：")
 out_file_path = input("分かち書きファイルパス：")
 
 with open(file_path) as f:
-    wakati_texts = [t.parse(line) for line in f]
+    wakati_texts_list = [t.parse(line).rstrip(" \n") + "\n" for line in f]  # .rstrip(" \n"):最後の文字の後ろのスペースを取り除く
+
+wakati_texts_list[-1] = wakati_texts_list[-1].rstrip("\n")  # 末尾は改行を削除
 
 with open(out_file_path, "w") as f2:
-    f2.writelines(wakati_texts)
+    f2.writelines(wakati_texts_list)
+
