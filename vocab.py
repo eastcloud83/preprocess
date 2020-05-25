8405ac860baa6b57b33c4b0b827c45adeadcc9b8
eastcloud83 <is0412xx@ed.ritsumei.ac.jp>
2020-05-25 14:25:38 +0900

    [fix] コードの書き方

--- vocab.py
+++ vocab.py
@@ -1,19 +1,16 @@
 # 分散表現が書かれたファイル
 vocab_file_path = input("単語分散表現ファイルパス：")
 
-vocab_list = []
-vocab_list.append("_PAD" + "\n")
-vocab_list.append("_UNK" + "\n")  # 未知語
+vocab_list = ["_PAD" + "\n", "_UNK" + "\n"]  # 単語を格納するリスト
 
-flag = False  # 1行目は追加しない
+flag = False  # 1行目は追加しない(形式の記述があるから)
 with open(vocab_file_path) as vf:
     for vocab_line in vf:
         if flag:
             vocab_list.append(vocab_line.split(" ")[0] + "\n")
         flag = True
 
-vocab_list[-1] = vocab_list[-1].rstrip("\n")  # 最後の単語は改行コード削除
+vocab_list[-1] = vocab_list[-1].rstrip("\n")  # 末尾の単語は改行を削除
 
-with open("vocab.txt", 'w') as f2:
-    for vocab in vocab_list:
-        f2.write(vocab)
+with open("vocab.txt", 'w') as f:
+    f.writelines(vocab_list)
